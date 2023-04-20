c058cc9a1b1da38cbb511352a8a342a48098b851
ShubhamBabar <121241828+ShubhamBabar@users.noreply.github.com>
2023-04-20 14:46:08 +0530

    Update Area_of_circle.java

--- Area_of_circle.java
+++ Area_of_circle.java
@@ -8,7 +8,7 @@ public class Area_of_circle {
 		System.out.println("Enter the Radius Of Circle :");  
 		float radius = SC.nextFloat();
 		 
-		double Area = 3.1416 * radius * radius;
+		double Area = 3.1416 * radius * radius; //Formula for area of circle is (3.1416 * r ** 2)
 		
 		System.out.println("Area of Circle is "+Area+".");		
 	}
