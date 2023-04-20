4382969c5862dc77fe88fcf38b87345cf2d60358
ShubhamBabar <121241828+ShubhamBabar@users.noreply.github.com>
2023-04-20 21:28:25 +0530

    done

--- Perimeter_of_parallelogram.java
+++ Perimeter_of_parallelogram.java
@@ -9,13 +9,13 @@ public class Perimeter_of_parallelogram {
 		   
 		   System.out.println("**Perimeter Of Parallelogram**");
 		   
-			System.out.print("Enter Height of Parallelogram (in cm): ");
-			float height =SC.nextFloat();
+			System.out.print("Enter width of Parallelogram (in cm): ");
+			float width =SC.nextFloat();
 			
-			System.out.print("Enter Base of Parallelogram (in cm): ");
-			float base =SC.nextFloat();
+			System.out.print("Enter length of Parallelogram (in cm): ");
+			float length =SC.nextFloat();
 			
-			double Perimeter =  2*(height + base);//Formula for Perimeter of Parallelogram is 2*(height + base) .
+			double Perimeter =  2*(width + length);//Formula for Perimeter of Parallelogram is 2*(width + length) .
 			
 			System.out.print("Perimeter of Parallelogram is " +Perimeter+" cm.");	
 
