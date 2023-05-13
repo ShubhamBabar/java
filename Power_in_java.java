f95e5cfef483735f650bc259b431e7036b406bd4
Shubham babar <121241828+ShubhamBabar@users.noreply.github.com>
2023-05-13 15:20:27 +0530

    done

---
+++ Power_in_java.java
@@ -0,0 +1,10 @@
+package javaProject4;
+
+public class Power_in_java {
+
+	public static void main(String[] args) {
+		double a=2,b=4;
+		double c = Math.pow(a, b); // return a^b i.e. 2^4 
+		System.out.println(c);
+	}
+}
